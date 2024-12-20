3f36f631639904bcd121883c25fe36ebfc43649e
NotManish <manishshrestha051@gmail.com>
2024-12-20 20:33:56 +0545

    Made the product container clickable so that it navigates to ProductDetails page

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,13 +1,13 @@
-// import './ProductCard.css';
-import { Link } from 'react-router-dom';
+
 import '../CSS/ProductCard.css';
 import { AppContext } from '../App';
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 function ProductCard({ productInfo }) {
-    const { AddToCart } = useContext(AppContext);
-
+    const { AddToCart, cartItem } = useContext(AppContext);
+    const navigate = useNavigate();
     // const AddToCart = () => {
     //     if (!cartItem.includes(productInfo)) {
     //         let newCartItem = productInfo;
@@ -18,19 +18,22 @@ function ProductCard({ productInfo }) {
     //         alert("This item is already added");
     //     }
     // }
+    const handleClick = (event, productInfo) => {
+        if (event.target.tagName == 'BUTTON') {
+            AddToCart(productInfo);
+        } else {
+            navigate('/product-details', { state: productInfo });
+        }
+    }
 
     return (
-        <div className="product-card">
+        <div className="product-card" onClick={(event) => { handleClick(event, productInfo); }}>
             <img src={productInfo.img} alt={productInfo.name} className="product-img" />
             <h3 className="product-name">{productInfo.title}</h3>
             <p className="product-price">Rs. {productInfo.price}</p>
-            <Link to="/product-details" state={productInfo} className="details-link">
-                Tap to see details...
-            </Link>
-
-
-
-            <button className="buy-button" onClick={() => AddToCart(productInfo)}>Add to Cart</button>
+            {
+                !cartItem.some(item => item.id === productInfo.id) && <button className="buy-button" >Add to Cart</button>
+            }
         </div>
     );
 }
