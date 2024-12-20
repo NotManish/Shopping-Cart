5871201d7794f1486aebcc0d0e4770026b59f765
NotManish <manishshrestha051@gmail.com>
2024-12-20 20:40:49 +0545

    Added functionality to'Add to Cart' button so that the product can be added to the cart from ProductDetails component

--- src/Components/ProductDetails.jsx
+++ src/Components/ProductDetails.jsx
@@ -4,11 +4,12 @@ import { useContext } from 'react';
 import { AppContext } from '../App';
 
 
+
 function ProductDetails() {
-  const{ AddToCart }=useContext(AppContext);
+  const { AddToCart, cartItem } = useContext(AppContext);
   const obj = useLocation();
-  const productInfo = obj.state; 
-    
+  const productInfo = obj.state;
+
   return (
     <div className='product_details'>
       <div className="product_container">
@@ -19,7 +20,11 @@ function ProductDetails() {
           <h1 className="product_title">{productInfo.title}</h1>
           <p className="product_price">Rs. {productInfo.price}</p>
           <p className="product_description_text">{productInfo.description}</p>
-          <button className="add_to_cart" onClick={()=>{AddToCart(productInfo);}}>Add to Cart</button>
+          {
+            !cartItem.some(item => item.id === productInfo.id) ?
+              <button className="add_to_cart" onClick={() => { AddToCart(productInfo); }}>Add to Cart</button> :
+              <h4>Item is added to the card</h4>
+          }
         </div>
       </div>
     </div>
